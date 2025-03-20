e8bafa0ec68b9e66e9b0d4408c0852356f9d3263
HafizSantosa <myhafizas@gmail.com>
2025-03-20 08:22:04 +0700

    feat: integrated database

--- flaskAPI.py
+++ flaskAPI.py
@@ -1,53 +1,62 @@
 from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo
+from bson import ObjectId
 
 app = Flask(__name__)
 
-# Dummy
-items = [
-    {"id": 1, "name": "Sepatu", "price": 10.0},
-    {"id": 2, "name": "Sandal", "price": 20.0}
-]
+app.config["MONGO_URI"] = "mongodb://localhost:27017/flaskdb"
+mongo = PyMongo(app)
+db = mongo.db.items
 
-# Read all
+# GET ALL
 @app.route('/items', methods=['GET'])
 def get_items():
+    items = []
+    for item in db.find():
+        items.append({"id": str(item["_id"]), "name": item["name"], "price": item["price"]})
     return jsonify(items)
 
-# Read ID
-@app.route('/items/<int:item_id>', methods=['GET'])
+# GET ID
+@app.route('/items/<string:item_id>', methods=['GET'])
 def get_item(item_id):
-    item = next((item for item in items if item["id"] == item_id), None)
+    item = db.find_one({"_id": ObjectId(item_id)})
     if item:
-        return jsonify(item)
+        return jsonify({"id": str(item["_id"]), "name": item["name"], "price": item["price"]})
     return jsonify({"error": "Item not found"}), 404
 
-# Create
+# POST
 @app.route('/items', methods=['POST'])
 def add_item():
-    new_item = request.json
-    new_item["id"] = len(items) + 1
-    items.append(new_item)
-    return jsonify(new_item), 201
+    data = request.json
+    new_item = {"name": data["name"], "price": data["price"]}
+    inserted_item = db.insert_one(new_item)
+    return jsonify({"id": str(inserted_item.inserted_id), "name": new_item["name"], "price": new_item["price"]}), 201
 
-# Update
-@app.route('/items/<int:item_id>', methods=['PUT'])
+# PUT
+@app.route('/items/<string:item_id>', methods=['PUT'])
 def update_item(item_id):
-    item = next((item for item in items if item["id"] == item_id), None)
+    item = db.find_one({"_id": ObjectId(item_id)})
     if not item:
         return jsonify({"error": "Item not found"}), 404
-    data = request.json
-    item.update(data)
-    return jsonify(item)
 
-# Delete
-@app.route('/items/<int:item_id>', methods=['DELETE'])
+    data = request.json
+    updated_data = {"$set": {"name": data.get("name", item["name"]), "price": data.get("price", item["price"])}}
+    db.update_one({"_id": ObjectId(item_id)}, updated_data)
+    return jsonify({"id": item_id, "name": data.get("name", item["name"]), "price": data.get("price", item["price"])})
+
+# DELETE
+@app.route('/items/<string:item_id>', methods=['DELETE'])
 def delete_item(item_id):
-    global items
-    items = [item for item in items if item["id"] != item_id]
+    item = db.find_one({"_id": ObjectId(item_id)})
+    if not item:
+        return jsonify({"error": "Item not found"}), 404
+
+    db.delete_one({"_id": ObjectId(item_id)})
     return jsonify({"message": "Item deleted"})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 #Endpoints
 # GET /items
